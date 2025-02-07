36857ca5d341e166b0ae0ec6c03866c32680c3c9
Liekkas-xlm <2548066685@qq.com>
2025-02-07 14:48:15 +0800

    多进程优化

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ q = -1.602 * 10 ** (-19)  # 电子电量
 h = 6.62607015 * 10 ** (-34)  # 普朗克常数
 m0 = 9.11 * 10 ** (-31)  # 电子质量
 m_eff = m0  # 先用电子质量代替
-sigma0 = 1  # 体电导率
+sigma0 = 59  # 体电导率
 
 
 # 矩形导线的SRFS模型
@@ -23,11 +23,7 @@ class SRFS:
         self.kapa_b = b
 
     def specular_reflection_scattering(self, xn, yn):
-
-        def func0(theta):
-            return 2 * np.pi * np.cos(theta) ** 2 * np.sin(theta)
-
-        int1, _ = integrate.quad(func0, 0, np.pi)
+        int1 = np.pi * 4 / 3
 
         def func1xy(fai, theta):
             epsilon = 1e-10
@@ -43,8 +39,8 @@ class SRFS:
             exp_kapa_b_neg_yn = exp_kapa_b ** (1 + yn)
 
             exp_kapa_a = np.exp(-self.kapa_a / (2 * denominator_a))
-            exp_kapa_a_xn = exp_kapa_b ** (1 - xn)
-            exp_kapa_a_neg_xn = exp_kapa_b ** (1 + xn)
+            exp_kapa_a_xn = exp_kapa_a ** (1 - xn)
+            exp_kapa_a_neg_xn = exp_kapa_a ** (1 + xn)
             # exp_kapa_b_yn = np.exp(
             #     np.clip(self.kapa_b * (yn - 1) / (2 * denominator_b), -700, 700)
             # )
@@ -78,7 +74,7 @@ class SRFS:
             lambda x: np.pi,
         )
         # 记录开始时间
-        start_time = time.time()
+        # start_time = time.time()
         int3 = 0
         for n in [1 + yn, 1 - yn]:
             for d in [1 + xn, 1 - xn]:
@@ -124,12 +120,14 @@ class SRFS:
                 )
                 int3 += result
         # 记录结束时间
-        end_time = time.time()
+        # end_time = time.time()
 
         # 计算运行时间
-        elapsed_time = end_time - start_time  # 将秒换算成分钟
-        print(f"积分3运行时间：{elapsed_time:.6f} s")
-
+        # elapsed_time = end_time - start_time  # 将秒换算成分钟
+        # print(f"积分3运行时间:{elapsed_time:.6f} s")
+        # print("int1 = ", int1)
+        # print("int2 = ", int2)
+        # print("int3 = ", int3)
         sigma = 3 / 4 * sigma0 * (int1 - int2 - int3) / np.pi
         return sigma
 
@@ -138,23 +136,28 @@ class SRFS:
         return self.specular_reflection_scattering(xn, yn)
 
 
+def compute_av_conductivity(param):
+    ka, kb, p = param
+    count = 0
+    conductivity_sum = 0.0
+    srfs_model = SRFS(p, ka, kb)
+    for xn in [0]:  # np.linspace(-1, 1, 10):
+        for yn in [0]:  # np.linspace(-1, 1, 10):
+            count += 1
+            conductivity_sum += srfs_model.pure_diffusion_surface_scatter(xn, yn)
+    return conductivity_sum / count
+
+
 def compute_conductivity_of_square_conductor():
-    # kapa = np.linspace(0, 20, 41)  # 生成kapa值
-    kapa = [0.5]
+    # kapa = np.linspace(0, 1, 3)  # 生成kapa值
+    kapa = [20]
     conductivity_list = []
     p = 0
-    srfs_model = SRFS(0, 0, 0)
     for ka in kapa:
-        count = 0
-        conductivity_sum = 0.0
         kb = ka
-        srfs_model.reset_params(0, ka, kb)
+        param = [ka, kb, p]
         print("运算 ka = ", ka)
-        for xn in np.linspace(-1, 1, 10):
-            for yn in np.linspace(-1, 1, 10):
-                count += 1
-                conductivity_sum += srfs_model.pure_diffusion_surface_scatter(xn, yn)
-        conductivity_list.append(conductivity_sum / count)
+        conductivity_list.append(compute_av_conductivity(param))
     # 可选：将结果保存到文件
     with open("output.txt", "w", encoding="utf-8") as file:
         file.write(
@@ -162,10 +165,44 @@ def compute_conductivity_of_square_conductor():
         )
         for item in conductivity_list:
             file.write(str(item) + "\n")
+    print("kapa对应的电导率 : ", conductivity_list)
     return kapa, conductivity_list
 
 
+"""使用多进程求解电导率"""
+
+
+def compute_conductivity_of_square_conductor_mp():
+    p_list = [0]
+    kapa = np.linspace(0, 20, 41)  # 生成kapa值
+    result_list = []
+    for p in p_list:
+        # 使用列表推导式生成所需的列表
+        params_list = [(k, k, p) for k in kapa]
+        # 进程数
+        num_processes = len(kapa)  # 进程数量等于区间数量
+        # 创建进程池
+        with multiprocessing.Pool(processes=num_processes) as pool:
+            # 使用 map 方法并行执行数值积分
+            results = pool.map(compute_av_conductivity, params_list)
+        # 打印结果
+        # print("所有进程完成,每个kapa的电导率:", results)
+
+        # 可选：将结果保存到文件
+        with open("output.txt", "w", encoding="utf-8") as file:
+            file.write(
+                "Conductivity versus κ for different specularity p = "
+                + str(p)
+                + " for square wire in the proposed SRFS model\n"
+            )
+            for item in results:
+                file.write(str(item) + "\n")
+        result_list.append(results)
+    return kapa, result_list, p_list
+
+
 def plot_conductivity_vs_kapa_square_wire(kapa, p_list, conductivitys):
+
     for conductivity, p in zip(conductivitys, p_list):
         plt.plot(kapa, conductivity, linewidth=2, label=f"p={p:.2f}")
     # 添加图例
@@ -176,10 +213,9 @@ def plot_conductivity_vs_kapa_square_wire(kapa, p_list, conductivitys):
 
 
 def main():
-    p = [0]
     # 记录开始时间
     start_time = time.time()
-    kapa, conductivity_list = compute_conductivity_of_square_conductor()
+    kapa, conductivity_list, p = compute_conductivity_of_square_conductor_mp()
     # 记录结束时间
     end_time = time.time()
 
